278818e50c6ae5cf95119b5e801db2923b320862
Igor Felipe <54617151+igor-felipe@users.noreply.github.com>
2023-03-03 22:56:26 -0300

    chore: update eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,27 @@
 module.exports = {
+  root: true,
   env: {
     es2021: true,
     node: true,
   },
   extends: [
-    'plugin:jest/recommended',
+    "plugin:jest/recommended",
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "eslint-config-airbnb-base", //you can omit `eslint-config-`
+    "eslint-config-airbnb-base", //you can omit "eslint-config-"
     "airbnb-typescript/base",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
+    ecmaVersion: 13,
+    sourceType: "module",
     project: "./tsconfig.json"
   },
   plugins: [
-    '@typescript-eslint',
+    "@typescript-eslint", 
   ],
   rules: {
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'always-multiline',
-    }],
-    camelcase: 'off',
-    'no-warning-comments': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    "import/extensions": [0, "never"]
   },
 }
