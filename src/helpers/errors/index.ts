87326bbdc29398d0d968f65140079dc7458616f5
Igor Felipe <54617151+igor-felipe@users.noreply.github.com>
2023-04-05 02:23:09 -0300

    refactor: validationError refactorin

--- src/helpers/errors/index.ts
+++ src/helpers/errors/index.ts
@@ -2,11 +2,11 @@ import { z } from "zod";
 
 export class ValidationError extends Error {
   details: {
-    [x: string]: string[] | undefined;
+    errors: { [x: string]: string[] | undefined };
   };
 
   constructor(zodError: z.ZodError) {
     super("ValidationError");
-    this.details = zodError.flatten().fieldErrors;
+    this.details = { errors: zodError.flatten().fieldErrors };
   }
 }
