ff7c4fdc25cc33def32382f6283d1e53b38467dd
Igor Felipe <54617151+igor-felipe@users.noreply.github.com>
2023-06-29 22:11:04 -0300

    feat: add validator for environment variables

---
+++ src/config/env-validator.ts
@@ -0,0 +1,43 @@
+import { pipe } from "fp-ts/function";
+import { z } from "zod";
+import * as TE from "fp-ts/TaskEither";
+import { EnvError } from "@/core/erros";
+
+const toEnvError = (error: unknown) => {
+  if (error instanceof z.ZodError) {
+    return new EnvError({ error: error.flatten().fieldErrors });
+  }
+  return new EnvError("Unknown error");
+};
+
+type Validator<T> = {
+  parse: (data: unknown) => T;
+};
+
+export const toTaskEither =
+  <T>(validator: Validator<T>) =>
+  (data: unknown) =>
+    TE.tryCatch(async () => validator.parse(data), toEnvError);
+
+const env = z.object({
+  JWT_SECRET: z
+    .string()
+    .min(32, { message: "JWT_SECRET must be at least 32 chars long" }),
+
+  JWT_EXPIRATION_TIME: z
+    .string()
+    .nonempty({ message: "JWT_EXPIRATION_TIME not set" }),
+  PORT: z.string().nonempty({ message: "PORT not set" }),
+
+  DATABASE_URL: z.string().nonempty({ message: "DATABASE_URL not set" }),
+});
+
+export const envValidator = pipe(
+  process.env,
+  toTaskEither(env),
+  TE.mapLeft((error) => {
+    throw error;
+  }),
+);
+
+envValidator();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
 import "./config/module-alias";
+import "./config/env-validator";
 
 import "./app";
