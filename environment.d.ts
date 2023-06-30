91ea9c045806d81cf68636fb7a36d7f5d85afa73
Igor Felipe <54617151+igor-felipe@users.noreply.github.com>
2023-06-29 21:21:09 -0300

    feat(add new variable): add new environment variables

--- environment.d.ts
+++ environment.d.ts
@@ -1,8 +1,10 @@
 declare global {
-  /* eslint-disable no-unused-vars */
   namespace NodeJS {
     interface ProcessEnv {
       PORT: string
+      JWT_SECRET: string
+      JWT_EXPIRATION_TIME: string
+      DATABASE_URL: string
     }
   }
 }
