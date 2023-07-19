5937fbb000dd00cd6913967df550973bef301d91
igor-felipe <54617151+igor-felipe@users.noreply.github.com>
2023-07-19 02:45:30 -0300

    feat: add use case adapter for resource and rule entities

---
+++ src/adapters/use-cases/resource_useCase_adapter.ts
@@ -0,0 +1,18 @@
+import * as resource from "@/core/resource/use-cases";
+import { db } from "@/container/db";
+
+export const create: resource.CreateUseCase = resource.create({
+  createInDb: db.resource.createInDb,
+});
+
+export const update: resource.UpdateUseCase = resource.update({
+  updateInDb: db.resource.updateInDb,
+});
+
+export const getOne: resource.GetOneUseCase = resource.getOne({
+  getOneInDb: db.resource.getOneInDb,
+});
+
+export const findMany: resource.FindManyUseCase = resource.findMany({
+  findManyInDb: db.resource.findManyInDb,
+});
---
+++ src/adapters/use-cases/rule_useCase_adapter.ts
@@ -0,0 +1,18 @@
+import * as rule from "@/core/rule/use-cases";
+import { db } from "@/container/db";
+
+export const create: rule.CreateUseCase = rule.create({
+  createInDb: db.rule.createInDb,
+});
+
+export const update: rule.UpdateUseCase = rule.update({
+  updateInDb: db.rule.updateInDb,
+});
+
+export const getOne: rule.GetOneUseCase = rule.getOne({
+  getOneInDb: db.rule.getOneInDb,
+});
+
+export const findMany: rule.FindManyUseCase = rule.findMany({
+  findManyInDb: db.rule.findManyInDb,
+});
